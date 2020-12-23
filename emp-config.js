1ed8b9806c2481ff2bef16893fc87d84e3f6dcd8
flyyuan <740004544@qq.com>
2020-12-23 16:25:31 +0800

    feat: delete var

--- emp-config.js
+++ emp-config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const packagePath = path.join(path.resolve('./'), 'package.json')
 const {dependencies} = require(packagePath)
 console.log(packagePath)
-const empConfig = require(`${resolveApp('')}/emp.json`);
+const empConfig = require(`${resolveApp('')}/emp.json`)
 module.exports = ({config, env}) => {
   const port = 8001
   const projectName = 'empReactBase'
@@ -11,14 +11,11 @@ module.exports = ({config, env}) => {
   config.output.publicPath(publicPath)
   // 设置项目端口
   config.devServer.port(port)
-  const remoteEntry = 'http://localhost:8002/emp.js'
   config.plugin('mf').tap(args => {
     args[0] = {
       ...args[0],
       ...{
         ...empConfig,
-        // 暴露项目的全局变量名
-        library: {type: 'var', name: empConfig.name},
         // 被远程引入的文件名
         filename: 'emp.js',
       },
@@ -33,9 +30,7 @@ module.exports = ({config, env}) => {
         // head 的 title
         title: 'EMP-Base-Project',
         // 远程调用项目的文件链接
-        files: {
-          js: [remoteEntry],
-        },
+        files: {},
       },
     }
     return args
